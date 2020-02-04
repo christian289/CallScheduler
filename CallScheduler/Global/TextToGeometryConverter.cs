a11d406d38c3c23b5325e2b153d4547069795689
이상준 <christian289@naver.com>
2020-02-04 18:06:48 +0900

    IValueConverter를 이용한 Path로 그림그리기 (Text를 입력받아 FormattedText 객체로 좌표정보를 받아서 Path 클래스로 직접 그리는 원리)

---
+++ CallScheduler/Global/TextToGeometryConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CallScheduler.Global
+{
+    [ValueConversion(typeof(Point[]), typeof(Geometry))]
+    public class TextToGeometryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string target = parameter.ToString();
+
+            Geometry obj = new FormattedText
+                (
+                    target,
+                    CultureInfo.CurrentCulture,
+                    FlowDirection.LeftToRight,
+                    new Typeface
+                    (
+                        new FontFamily("맑은 고딕"),
+                        FontStyles.Normal,
+                        FontWeights.Normal,
+                        FontStretches.Normal
+                    ),
+                    30,
+                    Brushes.Black,
+                    30
+                ).BuildGeometry(new Point(0, 0));
+
+            return obj;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CallScheduler/MainWindow.xaml.cs
+++ CallScheduler/MainWindow.xaml.cs
@@ -1,24 +1,5 @@
-﻿using CallScheduler.Global;
-using CallScheduler.Model;
-using CallScheduler.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CallScheduler.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml;
 
 namespace CallScheduler
 {
